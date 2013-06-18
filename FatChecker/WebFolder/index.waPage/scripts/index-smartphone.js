e7835d9ea571697a230a95fe2d3a640fbd1c075e
wretchard <wretchard@gmail.com>
2013-06-18 20:35:49 +1000

    additional

--- FatChecker/WebFolder/index.waPage/scripts/index-smartphone.js
+++ FatChecker/WebFolder/index.waPage/scripts/index-smartphone.js
@@ -2,12 +2,40 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var button1 = {};	// @button
 	var buttonCustomer = {};	// @button
 	var button_upload = {};	// @button
 	var button_show = {};	// @button
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+	button1.click = function button1_click (event)// @startlock
+	{// @endlock
+		checkfields();
+		
+	};// @lock
+	
+	function checkfields() {
+		if (!$$('textField1').getValue()) {
+			$$('richTextWarning').setValue('First Name missing')
+		}
+		else if (!$$('textField2').getValue()) {
+			$$('richTextWarning').setValue('Middle Name missing')
+		}
+		
+		else if (!$$('textField3').getValue()) {
+			$$('richTextWarning').setValue('Surname missing')
+		}
+		
+		else if (!$$('textField4').getValue()) {
+			$$('richTextWarning').setValue('User Name missing')
+		}
+		
+		else {
+			$$('richTextWarning').setValue('')
+			}
+	}
 
 	buttonCustomer.click = function buttonCustomer_click (event)// @startlock
 	{// @endlock
@@ -25,6 +53,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("button1", "click", button1.click, "WAF");
 	WAF.addListener("buttonCustomer", "click", buttonCustomer.click, "WAF");
 	WAF.addListener("button_upload", "click", button_upload.click, "WAF");
 	WAF.addListener("button_show", "click", button_show.click, "WAF");
