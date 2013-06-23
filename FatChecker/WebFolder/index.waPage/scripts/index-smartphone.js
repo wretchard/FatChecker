aa610ead24784244d0cc89a8ce60bb13cab1dcc4
wretchard <wretchard@gmail.com>
2013-06-23 21:21:07 +1000

    upload picture

--- FatChecker/WebFolder/index.waPage/scripts/index-smartphone.js
+++ FatChecker/WebFolder/index.waPage/scripts/index-smartphone.js
@@ -2,6 +2,8 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var selectType = {};	// @select
+	var documentEvent = {};	// @document
 	var login1 = {};	// @login
 	var button1 = {};	// @button
 	var buttonCustomer = {};	// @button
@@ -10,6 +12,43 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+function createUUID() {
+    // http://www.ietf.org/rfc/rfc4122.txt
+    var s = [];
+    var hexDigits = "0123456789abcdef";
+    for (var i = 0; i < 36; i++) {
+        s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+    }
+    s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+    s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+    s[8] = s[13] = s[18] = s[23] = "-";
+
+    var uuid = s.join("");
+    return uuid;
+}
+
+	selectType.change = function selectType_change (event)// @startlock
+	{// @endlock
+		document.getElementById('frame1-frame').contentDocument.getElementById('measureId').value=this.getValue();
+		document.getElementById('frame1-frame').contentDocument.getElementById('userNamer').value=WAF.directory.currentUser().userName;
+		varUuid =createUUID();
+		document.getElementById('frame1-frame').contentDocument.getElementById('Uuid').value=varUuid;
+		$$('richTextUUID').setValue(varUuid)
+	};// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+
+		if (WAF.directory.currentUser()) {
+			//alert(WAF.directory.currentUser().userName);
+			$$('navigationView1').goToView(1);
+		}
+		else {
+			$$('navigationView1').goToView(3);
+		}
+		
+	};// @lock
 
 	login1.logout = function login1_logout (event)// @startlock
 	{// @endlock
@@ -23,8 +62,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 
 	button1.click = function button1_click (event)// @startlock
 	{// @endlock
-		registerUser()
-		
+		registerUser();
 	};// @lock
 	
 	function registerUser() {
@@ -33,7 +71,12 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 			var isLong = checklengths();
 			if (comPlete && isLong) {
 			$$('richTextWarning').setValue('')
-			ds.Customer.registerCustomer(objRegistration);
+			ds.Customer.registerCustomer(objRegistration, {onSuccess: function(event) {
+				//$$('navigationView1').goToView(1)
+				$$('richTextWarning').setValue('You may now login')
+				}
+			
+		});
 		}
 		}		
 	}
@@ -89,10 +132,13 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 
 	button_show.click = function button_show_click (event)// @startlock
 	{// @endlock
-		$$('image1').setValue("images/nothingchanges.jpg")
+		vuuid=$$('richTextUUID').getValue();
+		sources.customerPicture.query("uuidvar=:1", vuuid)
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("selectType", "change", selectType.change, "WAF");
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
 	WAF.addListener("login1", "logout", login1.logout, "WAF");
 	WAF.addListener("login1", "login", login1.login, "WAF");
 	WAF.addListener("button1", "click", button1.click, "WAF");
